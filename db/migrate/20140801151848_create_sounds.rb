6ebd2288a460c2090ba22de98096544445011503
clai1018 <clai1018@gmail.com>
2014-08-01 11:34:42 -0400

    Created Sounds Controller

--- db/migrate/20140801151848_create_sounds.rb
+++ db/migrate/20140801151848_create_sounds.rb
@@ -1,8 +1,8 @@
 class CreateSounds < ActiveRecord::Migration
   def change
     create_table :sounds do |t|
-      t.string :category
-      t.text   :url
+      t.string   :category
+      t.text     :url
     end
   end
 end
