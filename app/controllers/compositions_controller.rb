fca80cc7389a2620a44c64ce0a2369daa89d7fa1
jonathanporras <jonathanrporras@gmail.com>
2014-08-03 19:54:06 -0400

    added create method to compositions_controller, added form to filter.js that adds comp to DB. defined /compositions/create method

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -1,7 +1,13 @@
 class CompositionsController < ApplicationController
 
   def create
-
+    sounds = []
+    sounds.push(Sound.find(params[:sound_id1]));
+    sounds.push(Sound.find(params[:sound_id2]));
+    sounds.push(Sound.find(params[:sound_id3]));
+    composition = Composition.create(name: params[:name])
+    composition.sounds << sounds
+    binding.pry
   end
 
   def comp_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'sounds#home'
+
+  #sounds routes
   get '/home', to: 'sounds#home'
   get '/filter', to: 'sounds#filter'
   get '/random', to: 'sounds#random'
   get '/canvas', to: 'sounds#canvas'
+
+  #compositions routes
+  get '/compositions/create', to: 'compositions#create'
 end
