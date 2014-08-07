88248cd0ff4d6a62ee52af7edcfc94ffeae79bc7
jonathanporras <jonathanrporras@gmail.com>
2014-08-07 17:45:44 -0400

    cleaned up unused commented code

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -8,10 +8,6 @@ class SoundsController < ApplicationController
     @user = User.new
     random = Sound.pluck(:url).sample(3)
     @sounds = random.map{|link| Sound.where(url: link).sample }
-    # @sounds = []
-    # random.each do |link|
-    #   @sounds.push(Sound.where(url: link).sample)
-    # end
   end
 
   def filter
@@ -19,14 +15,7 @@ class SoundsController < ApplicationController
     @sound1 = Sound.where(category: params[:category1]).sample(1)
     @sound2 = Sound.where(category: params[:category2]).sample(1)
     @sound3 = Sound.where(category: params[:category3]).sample(1)
-    # Sound.where(category: params[:category1])
-    # @sounds = Sound.filter_sounds(params)
   end
-
-  # def search
-  #   search_term = params[:search].downcase
-  #   @results = Sounds.where("LOWER(name) like ?", "%#(search_term)%")
-  # end
 
   def canvas
     @user = User.new
